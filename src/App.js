c3369d32722a5f42a346d7d6991b069ba7607f6e
Gersoncs107 <gerson.silva107@gmail.com>
2025-05-22 22:15:31 -0300

    courseinfo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 const App = () => {
-  const course = 'Half Stack application development'
 
-  const parts = [
-    {
-    name: 'Fundamentals of the React library',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'Component state',
-    exercises: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of the React library',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'Component state',
+        exercises: 14
+      }
+    ]
   }
-]
-  
+    
 return (
    <div>
     <Header course= {course}/>

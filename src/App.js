0f517e3673e33350580f106cd3e7788d9e881286
Gersoncs107 <gerson.silva107@gmail.com>
2025-05-30 22:58:26 -0300

    courseinfo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
+import { useState } from 'react'
+
 const App = () => {
 
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of the React library',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'Component state',
-        exercises: 14
-      }
-    ]
-  }
-    
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 return (
    <div>
-    <Header course= {course}/>
-    <Content parts={parts}/>
-    <Total parts={parts}/>
+    <h1>Give Feedback</h1>
+    <button onClick={() => setGood(good + 1)}>good</button>
+    <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+    <button onClick={() => setBad(bad + 1)}>bad</button>
+    <h1>Statistics</h1>
    </div> 
   )
 }

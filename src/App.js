384ee9149c429d799b3d850118ceb320c3f1af32
Gersoncs107 <gerson.silva107@gmail.com>
2025-06-02 23:46:38 -0300

    courseinfo

--- src/App.js
+++ src/App.js
@@ -13,7 +13,13 @@ const Total = () => {
 }
 
 const App = () => {
-
+  const course = 'Desenvolvimento de aplicação Half Stack'
+  const part1 = 'Fundamentos da biblioteca React'
+  const exercises1 = 10
+  const part2 = 'Usando props para passar dados'
+  const exercises2 = 7
+  const part3 = 'Estado de um componente'
+  const exercises3 = 14
 
 }
 

9e1520f4d6016ae7ff66f4e87f0f534c5b1a08ea
Gersoncs107 <gerson.silva107@gmail.com>
2025-06-12 20:03:40 -0300

    anecdotes

--- src/App.js
+++ src/App.js
@@ -19,21 +19,64 @@ const Total = ({ parts }) => {
   )
 }
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    { name: 'Fundamentals of the React library', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'Component state', exercises: 14 }
-  ]
-
+const Course = ({course}) => {
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />    
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+
+  return <Course course={course} />
 }
 
 export default App

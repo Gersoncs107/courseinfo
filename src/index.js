8f333427bc82bdbf72f45ac6299bc318c2fd266c
Gersoncs107 <gerson.silva107@gmail.com>
2025-06-21 22:49:31 -0300

    phonebook

--- src/index.js
+++ src/index.js
@@ -4,29 +4,31 @@ import App from "./App"
 import AppTest from "./AppTest"
 
 const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
-
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-]
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+.then((response) => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById("root")).render(
   <AppTest notes={notes}/>
 )
+})
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only JavaScript",
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true,
+//   },
+// ]
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <AppTest notes={notes}/>
+// )

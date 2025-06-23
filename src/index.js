647a4b7c87bace6ea6e79b973e32b31ee43e0467
Gersoncs107 <gerson.silva107@gmail.com>
2025-06-22 22:15:17 -0300

    phonebook

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const promise = axios.get('http://localhost:3001/notes')
 .then((response) => {
   const notes = response.data
   ReactDOM.createRoot(document.getElementById("root")).render(
-  <AppTest notes={notes}/>
+  <AppTest/>
 )
 })
 

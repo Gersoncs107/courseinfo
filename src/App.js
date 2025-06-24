4d0285e46ec7d1b7f0aac6994460d40c98747958
Gersoncs107 <gerson.silva107@gmail.com>
2025-06-23 22:33:59 -0300

    phonebook

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import { useState, useEffect } from 'react'
 import Persons from './components/Persons'
 import PersonsForm from './components/PersonsForm'
 import Filter from './components/Filter'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filteredPersons, setFilteredPersons] = useState(persons);
   const [filter, setFilter] = useState('')
 
+  // Fetch persons from server once
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then((response) => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  // Update filteredPersons whenever persons or filter changes
   useEffect(() => {
     setFilteredPersons(
       persons.filter(person =>

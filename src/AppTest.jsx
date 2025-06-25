491b33d998aaf2e93e31904794048503c6ea85e1
Gersoncs107 <gerson.silva107@gmail.com>
2025-06-24 22:39:33 -0300

    phonebook

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -22,8 +22,13 @@ const AppTest = () => {
             important: Math.random() < 0.5,
             id: notes.length + 1
         }
-        setNotes(notes.concat(noteObject))
-        setNewNote('')
+
+        axios
+        .post('http://localhost:3001/notes/', noteObject)
+        .then((response) => {
+            console.log(response)
+        })
+
     }
 
     const handleNoteChange = (event) => {

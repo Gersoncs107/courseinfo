4be3bad6a23801c805500b7e0aa79c4b3d0ca0bd
Gersoncs107 <gerson.silva107@gmail.com>
2025-06-26 22:39:41 -0300

    phonebook

--- src/services/notes.js
+++ src/services/notes.js
@@ -8,3 +8,13 @@ const getAll = () => {
 const create = (newObject) => {
     return axios.post(baseUrl, newObject)
 }
+
+const update = (id, newObject) => {
+    return axios.put(`${baseUrl}/${id}`, newObject)
+}
+
+export default {
+    getAll: getAll,
+    create: create,
+    update: update
+}

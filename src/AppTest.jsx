fe48ba76197bfeb14f912756a51e058b2d6c04eb
Gersoncs107 <gerson.silva107@gmail.com>
2025-06-27 22:53:05 -0300

    phonebook

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -31,15 +31,16 @@ const AppTest = () => {
 
     }
 
-    const toggleImportanceOf = (id)=> {
-        const url = `http://localhost:3001/notes/${id}`
-        const note = notes.find( n => n.id === id)
-        const changedNote = {...note, important: !note.important}
+     const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
 
-       .then(returnedNote => {
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
-    }
+  }
 
     
 
